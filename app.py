501351cca4aaa32b9b6f376b502f85e984c2490d
Venkateswarao Tella <venkateswaraotella25@gmail.com>
2025-10-21 14:09:52 +0530

    Updated logic

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 @app.route('/sum', methods=['GET'])
 def get_sum():
     a = int(request.args.get('a', 0))
+    print(a)
     b = int(request.args.get('b', 0))
     return jsonify({"sum": a + b})
 
